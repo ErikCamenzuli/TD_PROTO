cf5ffbccd6b3652fecac53e4e1e0e0477b90727b
Erik Camenzuli <erik.camenzuli@hotmail.com>
2024-12-04 04:13:23 +1100

    Commenting Code

--- TD/Assets/Scripts/3D_SCRIPTS/AI/Spawner.cs
+++ TD/Assets/Scripts/3D_SCRIPTS/AI/Spawner.cs
@@ -9,7 +9,7 @@ public class Spawner : MonoBehaviour
     public Transform[] spawnPoints;
     public Transform[] wayPoints;
     public int enemiesPerwave = 5;
-    public float timeBetweenWabes = 5f;
+    public float timeBetweenWaves = 5f;
     public float spawnDelay = 0.5f;
 
     private int currentWave = 1;
@@ -18,6 +18,7 @@ public class Spawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Checks to see if there's a wave spawnning, if not then the wave will start
         if(!isSpawning)
         {
             StartCoroutine(SpawnWave());
@@ -26,28 +27,41 @@ public class Spawner : MonoBehaviour
 
     private IEnumerator SpawnWave()
     {
+        //Activate the spawning of the wave
         isSpawning = true;
 
+        //Looping how many enemies per wave there is  
         for(int i = 0; i < enemiesPerwave; i++)
         {
+            //Spawns enemy
             SpawnEnemy();
+            //Waiting between each spawn
             yield return new WaitForSeconds(spawnDelay);
         }
 
-        yield return new WaitForSeconds(timeBetweenWabes);
+        //Waiting between each wave
+        yield return new WaitForSeconds(timeBetweenWaves);
+        //Increment current wave by 1
         currentWave++;
+        //Adding 2 each enemies on each new wave
         enemiesPerwave += 2;
+        //Disabling the spawning on the wave
         isSpawning = false;
 
     }
 
     private void SpawnEnemy()
     {
+        //Setting the array up for the position of the spawn point(s)
         Transform spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)];
 
+        //Setting the enemys gameobject to the prefab, the position of the spawn point and setting no rotation to the prefab
         GameObject enemy = Instantiate(enemyPrefab, spawnPoint.position, Quaternion.identity);
+        //Setting enemy script to the enemys component for waypoints
         WayPoints enemyScript = enemy.GetComponent<WayPoints>();
 
+        //Checking to see if the enemies script is null then setting it to
+        //the waypoints
         if(enemyScript != null)
         {
             enemyScript.wayPoints = wayPoints;
--- TD/Assets/Scripts/3D_SCRIPTS/AI/WayPoints.cs
+++ TD/Assets/Scripts/3D_SCRIPTS/AI/WayPoints.cs
@@ -11,27 +11,35 @@ public class WayPoints : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Checking to see if there are any way points then return nothing
         if(wayPoints == null || wayPoints.Length == 0)
         {
             //Debug.LogWarning("NO WAYPOINTS ASSIGNED!");
             return;
         }
 
+        //Setting the target way point to a way points ID  
         Transform targetWayPoint = wayPoints[currentWayPointId];
+        //Setting the direction for the targets position
         Vector3 dir = (targetWayPoint.position - transform.position).normalized;
+        //Setting the positions with a direction * the move speed for the AI 
         transform.position += dir * moveSpeed * Time.deltaTime;
 
+        //Checking if the distance is less than .1
         if (Vector3.Distance(transform.position, targetWayPoint.position) < 0.1f)
         {
+            //Increment by 1
             currentWayPointId++;
 
+            //Checking to see if the enemy is at the last way point
             if(currentWayPointId >= wayPoints.Length)
             {
+                //Destroy the enmy regardless (for now)
                 Destroy(gameObject);
             }
         }
     }
-
+    //Just some drawings
     private void OnDrawGizmo()
     {
         if(wayPoints == null || wayPoints.Length == 0)
